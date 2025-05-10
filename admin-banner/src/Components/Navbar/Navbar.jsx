947ad31e5c7b7530527d93354476b9787589c18f
mercysound <adekunleadewolemercy@gmail.com>
2025-05-10 17:07:33 +0100

    somithi

--- admin-banner/src/Components/Navbar/Navbar.jsx
+++ admin-banner/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Navbar.css'
 // import navlogo from '../../assets/';
-// import navlogo from '../../assets/nav-logo.svg';
+import navlogo from '../../assets/nav-logo.svg';
 import navProfile from '../../assets/nav-profile.svg';
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      {/* <img src={navlogo} alt='' className='nav-logo'/> */}
+      <img src={navlogo} alt='' className='nav-logo'/>
       <img src={navProfile} alt='' className='nav-profile'/>
       
     </div>
